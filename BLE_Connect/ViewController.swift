c1fd8b784f41a3f3aa3f7cb349645504a52e3d27
服部翼 <tsuba-h@arsaga.jp>
2020-06-27 01:04:43 +0900

    Initial Commit

---
+++ BLE_Connect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLE_Connect
+//
+//  Created by 服部　翼 on 2020/06/27.
+//  Copyright © 2020 服部　翼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
